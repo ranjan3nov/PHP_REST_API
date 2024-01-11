afae7f483bb0d6a0e4982df9771d9252b35defac
ranjan3nov <ranjan3nov@gmail.com>
2024-01-11 16:36:27 +0530

    Intial Commit

---
+++ Database.php
@@ -0,0 +1,35 @@
+<?php
+class Database
+{
+    private $host = "localhost";
+    private $username = 'root';
+    private $password = '';
+    private  $db_name = 'ercmax_ventures';
+    public $conn;
+
+    public function __construct()
+    {
+        $this->connect();
+    }
+
+    private function connect()
+    {
+        try {
+            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+        } catch (Exception $e) {
+            die("DB Connection Failed :" . $e->getMessage());
+        }
+    }
+    public function query($sql)
+    {
+        try {
+            return $this->conn->query($sql);
+        } catch (Exception $e) {
+            die("Sql Error :" . $e->getMessage());
+        }
+    }
+    public function close()
+    {
+        return $this->conn->close();
+    }
+}
